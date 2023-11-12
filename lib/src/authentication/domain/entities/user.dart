3dfc273a3eba1ece6ac54302218f71c474e35c7e
Affaf_Istibshar <afafistibsharazhar@gmail.com>
2023-11-12 01:34:47 -0800

    Setting up domain auth!

---
+++ lib/src/authentication/domain/entities/user.dart
@@ -0,0 +1,12 @@
+class User {
+  final int id;
+  final String name;
+  final String createdAt;
+  final String avatar;
+
+  User(
+      {required this.id,
+      required this.name,
+      required this.createdAt,
+      required this.avatar});
+}
