af640ff025435a59b64e23d318b92c5c4a788e6f
Affaf_Istibshar <afafistibsharazhar@gmail.com>
2023-11-13 21:59:07 -0800

    Usecases based testing!

--- test/src/authentication/domain/usecases/get_users_test_test.dart
+++ test/src/authentication/domain/usecases/get_users_test_test.dart
@@ -1,7 +1,9 @@
- import 'package:flutter_test/flutter_test.dart';
+ import 'package:dartz/dartz.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:testdrivndevelopment/src/authentication/domain/entities/user.dart';
 import 'package:testdrivndevelopment/src/authentication/domain/repository/auth_repos.dart';
 import 'package:testdrivndevelopment/src/authentication/domain/usecases/get_users.dart';
-
+import 'package:mocktail/mocktail.dart';
 import 'authentication_repo.mock.dart';
 
 void main (){
@@ -12,5 +14,18 @@ void main (){
    repository = MockAuthenticationRepository();
    usecase = GetUsers(repository);
   });
-  test(('Should'), () => null)
- }
+  final tResponse = [const User.empty()];
+
+  test(('Should call  the [AuthRepo.getUsers] and it should return lit of users'),
+          () async {
+    //Arrange
+    when(()=> repository.getUsers()).
+    thenAnswer((_) async =>  Right(tResponse));
+    //Act
+        final result = await usecase();
+        expect(result, equals( Right<dynamic, List<User>>(tResponse)));
+        verify(()=> repository.getUsers()).called(1);
+        verifyNoMoreInteractions(repository);
+
+  });
+}
